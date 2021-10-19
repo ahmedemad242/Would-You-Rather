ee1636c0043b53178308b67cb2d843c5dfa3c9f4
ahmedemad242 <ahmedemad242@gmail.com>
2021-10-19 20:42:02 +0200

    Feat: store is now setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
+import { Provider } from 'react-redux'
+
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider>,
   document.getElementById('root')
 );
